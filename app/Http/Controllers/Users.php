d9634916bccffe3c2540cf5be7206a164f9c5b61
xmaster06 <xmaster06@hotmail.com>
2020-10-30 21:52:55 +0300

    Learn [HtmlForm][2]

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class Users extends Controller
 {
     public function index(Request $req){
+        $req->validate([
+            'username'  =>  'required | max:10',
+            'password'  =>  'required | min:5'
+        ]);
         return $req->input();
     }
 }
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,8 +1,11 @@
 <h1>User Login</h1>
 
+
 <form method="post" action="users">
     @csrf
-    <input type="text" name="username"/>
-    <input type="password" name="password"/>
+    <input type="text" name="username" />
+    <span style="color:red">@error('username'){{$message}}@enderror</span><br />
+    <input type="password" name="password" />
+    <span style="color:red">@error('password'){{$message}}@enderror</span><br />
     <button type="submit">Login</button>
 </form>
